def70f6f2ae1692ff0915b289a171c459ab8f84f
Niklas Klasén <niklasklasen@evry.com>
2020-10-20 23:59:09 +0200

    completed more of the training.

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,7 +6,22 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas");
+            var numbers = new[] { 3, 6, 9 };
+            foreach (var number in numbers)
+            {
+                var result =+ number;
+            }
+            var sum = numbers[0] + numbers[1] + numbers[2];
+            System.Console.WriteLine(sum);
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
