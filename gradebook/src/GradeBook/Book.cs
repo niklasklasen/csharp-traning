3c6a5c44cacf1f800ea02189f01103d70e49081c
Niklas Klasén <niklasklasen@evry.com>
2020-10-22 23:38:14 +0200

    module completed

---
+++ gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The avrage is {result:N1}");
+            System.Console.WriteLine($"The lowest is {lowGrade:N1}");
+            Console.WriteLine($"The highest is {highGrade:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+}
