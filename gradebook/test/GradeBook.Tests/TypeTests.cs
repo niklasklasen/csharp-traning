771f2e78d9d16f43679646d0b78bf8758dfe52f8
Niklas Klasén <niklasklasen@evry.com>
2020-10-25 00:11:19 +0200

    module completed

--- gradebook/test/GradeBook.Tests/TypeTests.cs
+++ gradebook/test/GradeBook.Tests/TypeTests.cs
@@ -67,6 +67,21 @@ namespace GradeBook.Tests
         }
 
         [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Niklas";
+            var upper = MakeUpperCase(name);
+
+            Assert.Equal("Niklas", name);
+            Assert.Equal("NIKLAS", upper);
+        }
+
+        private string MakeUpperCase(string parameter)
+        {
+            return parameter.ToUpper();
+        }
+
+        [Fact]
         public void GetBookReturnsDifferentObjects()
         {
             var book1 = GetBook("Book 1");
